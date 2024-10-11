30f41d32bb20202bd4dc04fd278f417288ac300d
anMois <kew1672@naver.com>
2024-10-11 17:33:15 +0900

    [Feat] 이미지에 대한 오브젝트 자료형 변경
    기존에는 하나의 오브젝트만 생성했다면 여러 이미지를 통해 해당 이미지에 맞는 오브젝트를 생성하기 위해 생성하는 오브적트의 정리가 필요할 것 같아 List<GameObject>로 자료형을 변경

--- Assets/Scripts/TestImageTracker.cs
+++ Assets/Scripts/TestImageTracker.cs
@@ -6,11 +6,20 @@ public class TestImageTracker : MonoBehaviour
 {
     [SerializeField] ARTrackedImageManager imageManager;
 
-    [SerializeField] GameObject objPrefab;
-    [SerializeField] GameObject magicianprefab;
-    [SerializeField] float timer;
-    [SerializeField] float checkTime;
+    [SerializeField] List<GameObject> objPrefabList = new List<GameObject>();
+    [SerializeField] Dictionary<string, GameObject> objDicList = new Dictionary<string, GameObject>();
     [SerializeField] List<ARTrackedImage> trackImageList = new List<ARTrackedImage>();
+    [SerializeField] float checkTime;
+    [SerializeField] float timer;
+
+    private void Awake()
+    {
+        foreach (GameObject obj in objPrefabList)
+        {
+            string name = obj.name;
+            objDicList.Add(name, obj);
+        }
+    }
 
     private void OnEnable()
     {
@@ -46,41 +55,38 @@ public class TestImageTracker : MonoBehaviour
 
     private void OnImageChange(ARTrackedImagesChangedEventArgs args)
     {
-        //새로운 이미지가 추적되었을 때
         foreach (ARTrackedImage trackedImage in args.added)
         {
-            //이미지 라이브러리에서 이미지의 이름을 확인
             string imageName = trackedImage.referenceImage.name;
 
-            //새로운 게임오브젝트를 트래킹한 이미지의 자식으로 생성
             switch (imageName)
             {
                 case "Y Bot":
-                    GameObject dragon = Instantiate(objPrefab, trackedImage.transform.position, trackedImage.transform.rotation);
+                    GameObject dragon = Instantiate(GetObjPrefab(imageName), trackedImage.transform.position, trackedImage.transform.rotation);
                     dragon.transform.parent = trackedImage.transform;
                     trackImageList.Add(trackedImage);
-                    break;
-                case "Magician":
-                    GameObject magician = Instantiate(magicianprefab, trackedImage.transform.position, trackedImage.transform.rotation);
-                    magician.transform.parent = trackedImage.transform;
                     break;
             }
         }
 
-        //기존의 이미지가 변경(이동, 회전) 되었을 때
         foreach (ARTrackedImage trackedImage in args.updated)
         {
-            //이미지의 변셩사항이 있는 경우 자식으로 있건 게임오브젝트를 위치와 회전을 갱신
             if (trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking)
             {
                 trackedImage.transform.GetChild(0).position = trackedImage.transform.position;
                 trackedImage.transform.GetChild(0).rotation = trackedImage.transform.rotation;
                 trackedImage.gameObject.SetActive(true);
+                timer = 0;
             }
             else
             {
                 trackedImage.gameObject.SetActive(false);
             }
         }
+    }
+
+    private GameObject GetObjPrefab(string str)
+    {
+        return objDicList[str];
     }
 }
